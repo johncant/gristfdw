867c87724937eb1a4d8f6e8ffdd6db83a445651b
John Cant <a.johncant@gmail.com>
2023-06-17 22:51:16 +0100

    Split out fixtures and constants from tests

---
+++ test/defaults.py
@@ -0,0 +1,9 @@
+# These settings are associated with data in test/fixtures/grist_persist
+GRIST_API_KEY="23c5ae3adcda4890d2d8c51faddbe12bdd4b36a9"
+GRIST_DOC_ID="qS2YtmEG67cFjbgBBaooFQ"
+GRIST_SERVER_FROM_POSTGRES="http://grist:8484"
+GRIST_SERVER_FROM_LOCAL="http://localhost:8484"
+POSTGRES_USER="gristfdw" # TODO - DRY
+POSTGRES_PASSWORD="secret" # TODO - DRY
+POSTGRES_HOST="localhost"
+POSTGRES_PORT=5433
